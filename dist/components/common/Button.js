14721e73554c1d0519f6682209b2d7c87a3c10d2
ortutay <marcell.ortutay@gmail.com>
2024-10-21 15:20:16 -0700

    test publish 3

--- dist/components/common/Button.js
+++ dist/components/common/Button.js
@@ -10,6 +10,7 @@ var _Loading = require("./Loading");
 var _constants = require("../../constants");
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _extends() { return _extends = Object.assign ? Object.assign.bind() : function (n) { for (var e = 1; e < arguments.length; e++) { var t = arguments[e]; for (var r in t) ({}).hasOwnProperty.call(t, r) && (n[r] = t[r]); } return n; }, _extends.apply(null, arguments); }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
@@ -21,39 +22,28 @@ var Button = exports.Button = function Button(props) {
     _useState2 = _slicedToArray(_useState, 2),
     width = _useState2[0],
     setWidth = _useState2[1];
-  return /*#__PURE__*/_react["default"].createElement("div", null, "BUTTON");
-
-  // const buttonRef = useRef(null);
-
-  // useEffect(() => {
-  //   if (buttonRef.current) {
-  //     setWidth(buttonRef.current.offsetWidth);
-  //   }
-  // }, [props.loading]);
-
-  // const extraStyles = {};
-  // if (props.loading) {
-  //   extraStyles.width = width;
-  //   extraStyles.opacity = 1;
-  //   extraStyles.cursor = 'default';
-  // }
-
-  // return (
-  //   <button
-  //     ref={buttonRef}
-  //     style={extraStyles}
-  //     {...props}>
-
-  //     {!props.loading && props.children}
-  //     {props.loading && (
-  //       <div style={{ marginTop: 1 }}>
-  //       <Loading
-  //       size={(props?.className || '').indexOf('bt-sm') == -1 ? 18 : 14}
-  //       color={(props?.className || '').indexOf('outline') == -1 ? 'white' : primaryColor}
-  //       />
-  //       </div>)
-  //      }
-  //   </button>
-  // );
+  var buttonRef = (0, _react.useRef)(null);
+  (0, _react.useEffect)(function () {
+    if (buttonRef.current) {
+      setWidth(buttonRef.current.offsetWidth);
+    }
+  }, [props.loading]);
+  var extraStyles = {};
+  if (props.loading) {
+    extraStyles.width = width;
+    extraStyles.opacity = 1;
+    extraStyles.cursor = 'default';
+  }
+  return /*#__PURE__*/_react["default"].createElement("button", _extends({
+    ref: buttonRef,
+    style: extraStyles
+  }, props), !props.loading && props.children, props.loading && /*#__PURE__*/_react["default"].createElement("div", {
+    style: {
+      marginTop: 1
+    }
+  }, /*#__PURE__*/_react["default"].createElement(_Loading.Loading, {
+    size: ((props === null || props === void 0 ? void 0 : props.className) || '').indexOf('bt-sm') == -1 ? 18 : 14,
+    color: ((props === null || props === void 0 ? void 0 : props.className) || '').indexOf('outline') == -1 ? 'white' : _constants.primaryColor
+  })));
 };
 //# sourceMappingURL=Button.js.map
