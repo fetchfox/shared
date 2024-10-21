3aba21e403b73764b2b5c1b190d27b89c3036c54
ortutay <marcell.ortutay@gmail.com>
2024-10-21 15:19:33 -0700

    test publish 2

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -4,38 +4,36 @@ import { primaryColor } from '../../constants';
 
 export const Button = (props) => {
   const [width, setWidth] = useState(null);
-  return <div>BUTTON</div>;
+  const buttonRef = useRef(null);
 
-  // const buttonRef = useRef(null);
+  useEffect(() => {
+    if (buttonRef.current) {
+      setWidth(buttonRef.current.offsetWidth);
+    }
+  }, [props.loading]);
 
-  // useEffect(() => {
-  //   if (buttonRef.current) {
-  //     setWidth(buttonRef.current.offsetWidth);
-  //   }
-  // }, [props.loading]);
+  const extraStyles = {};
+  if (props.loading) {
+    extraStyles.width = width;
+    extraStyles.opacity = 1;
+    extraStyles.cursor = 'default';
+  }
 
-  // const extraStyles = {};
-  // if (props.loading) {
-  //   extraStyles.width = width;
-  //   extraStyles.opacity = 1;
-  //   extraStyles.cursor = 'default';
-  // }
+  return (
+    <button
+      ref={buttonRef}
+      style={extraStyles}
+      {...props}>
 
-  // return (
-  //   <button
-  //     ref={buttonRef}
-  //     style={extraStyles}
-  //     {...props}>
-
-  //     {!props.loading && props.children}
-  //     {props.loading && (
-  //       <div style={{ marginTop: 1 }}>
-  //       <Loading
-  //       size={(props?.className || '').indexOf('bt-sm') == -1 ? 18 : 14}
-  //       color={(props?.className || '').indexOf('outline') == -1 ? 'white' : primaryColor}
-  //       />
-  //       </div>)
-  //      }
-  //   </button>
-  // );
+      {!props.loading && props.children}
+      {props.loading && (
+        <div style={{ marginTop: 1 }}>
+        <Loading
+        size={(props?.className || '').indexOf('bt-sm') == -1 ? 18 : 14}
+        color={(props?.className || '').indexOf('outline') == -1 ? 'white' : primaryColor}
+        />
+        </div>)
+       }
+    </button>
+  );
 }
