b38fc825aaa6cff102dc73b2342b0c2c8b4c8d3c
ortutay <marcell.ortutay@gmail.com>
2024-10-21 14:51:05 -0700

    jsx + sourcemaps

---
+++ src/MyComponent2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const MyComponent2  = () => {
+  error();
+  return (
+    <div>JSX transpile</div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export const test = 'test from ui7';
 
 export { MyComponent } from './MyComponent.js';
+export { MyComponent2 } from './MyComponent2.js';
